654e669e0c145e55534775f378de3720d4de1303
Karlduino <karlduino@gmail.com>
2012-09-22 17:17:34 -0500

    Add a bit of animation

--- HalloweenPumpkin.ino
+++ HalloweenPumpkin.ino
@@ -5,12 +5,12 @@
  *   http://www.ladyada.net/make/pumpkin/pumpkin.html
  *
  * Karlduino, September, 2012
- * 
+ *
  * licensed under GPL 3
  **********************************************************************/
 
 const int pirPin = 19;
-const int turnOffTime = 1000;
+const int turnOffTime = 0;
 long lastPIR=0, curTime;
 int PIRreading;
 
@@ -24,12 +24,7 @@ const int mouth[3]= {17, 16, 15}; // center, middle, outer
 void setup(void)
 {
   pinMode(pirPin, INPUT);
-  pinMode(eyes, OUTPUT);
-  digitalWrite(eyes, LOW);
-  for(int i=0; i<3; i++) {
-    pinMode(mouth[i], OUTPUT);
-    digitalWrite(eyes, LOW);
-  }
+  faceSetup();
 }
 
 
@@ -46,15 +41,33 @@ void loop(void) {
   }
 
   if(PIRstate==0) {
-    digitalWrite(eyes, LOW);
-    for(int i=0; i<3; i++) {
-      digitalWrite(mouth[i], LOW);
-    }
+    faceOff();
   }
   else {
-    digitalWrite(eyes, HIGH);
-    for(int i=0; i<3; i++) {
-      digitalWrite(mouth[i], HIGH);
-    }
+    faceOn();
   }
+}
+
+
+void faceSetup() {
+  pinMode(eyes, OUTPUT);
+  for(int i=0; i<3; i++)
+    pinMode(mouth[i], OUTPUT);
+  faceOff();
+}
+
+void faceOff() {
+  digitalWrite(eyes, LOW);
+  for(int i=2; i>=0; i--) {
+    delay(750);
+    digitalWrite(mouth[i], LOW);
+  }
+}
+
+void faceOn() {
+  for(int i=0; i<3; i++) {
+    digitalWrite(mouth[i], HIGH);
+    delay(750);
+  }
+  digitalWrite(eyes, HIGH);
 }
