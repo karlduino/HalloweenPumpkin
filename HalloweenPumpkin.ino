4849bcde555045c980f88febaa39860d23f1d51e
Karlduino <karlduino@gmail.com>
2012-09-28 22:32:23 -0500

    Add use of wave shield for sounds

--- HalloweenPumpkin.ino
+++ HalloweenPumpkin.ino
@@ -9,27 +9,54 @@
  * licensed under GPL 3
  **********************************************************************/
 
+#include <WaveHC.h>
+#include <WaveUtil.h>
+
 const int pirPin = 19;
 const int turnOffTime = 0;
 long lastPIR=0, curTime;
 int PIRreading;
 
 int PIRstate = 0;
+int lastPIRstate = 1;
 
+const int eyes=6;
+const int mouth[3]= {7, 8, 9}; // center, middle, outer
+const int volumeInput = 1;
 
-const int eyes=18;
-const int mouth[3]= {17, 16, 15}; // center, middle, outer
+SdReader card;  // card
+FatVolume vol;  // partition on card
+FatReader root; // root directory on partition
+FatReader file; // file on card
+WaveHC wave;    // wave audio object
+
+const int numFiles = 7;
+char files[numFiles][13] = {"cackle.wav",   "catscrem.wav", "ghoullaf.wav",
+                            "gostmoan.wav", "scream.wav",   "scream2.wav",
+                            "screech.wav" };
+
+const int numWelcomeFiles = 2;
+char welcomeFiles[numWelcomeFiles][13] = {"hpyhalwn.wav", "welcome.wav" };
 
 
 void setup(void)
 {
   pinMode(pirPin, INPUT);
+  pinMode(volumeInput, INPUT);
   faceSetup();
+
+  // initialize card
+  card.init();
+  vol.init(card);
+  root.openRoot(vol);
+
+  randomSeed(analogRead(0));
 }
 
 
 void loop(void) {
   curTime = millis();
+  lastPIRstate = PIRstate;
   PIRreading = digitalRead(pirPin);
 
   if(PIRreading==HIGH) {
@@ -41,10 +68,22 @@ void loop(void) {
   }
 
   if(PIRstate==0) {
-    faceOff();
+    faceOff(0);
   }
   else {
-    faceOn();
+    eyesOn();
+    if(lastPIRstate == 0) {
+      playfile(welcomeFiles[random(numWelcomeFiles)]);
+    }
+    else {
+      playfile(files[random(numFiles)]);
+    }
+
+    while(wave.isplaying) {// playing occurs in interrupts, so we print dots in realtime
+      animateMouth(getVolume(volumeInput));
+    }
+    mouthOff(0);
+    delay(5000 + random(10000));
   }
 }
 
@@ -53,21 +92,66 @@ void faceSetup() {
   pinMode(eyes, OUTPUT);
   for(int i=0; i<3; i++)
     pinMode(mouth[i], OUTPUT);
-  faceOff();
+  faceOff(0);
 }
 
-void faceOff() {
+void faceOff(int delayAmount) {
   digitalWrite(eyes, LOW);
+  mouthOff(delayAmount);
+}
+
+void mouthOff(int delayAmount) {
   for(int i=2; i>=0; i--) {
-    delay(750);
+    delay(delayAmount);
     digitalWrite(mouth[i], LOW);
   }
 }
 
-void faceOn() {
-  for(int i=0; i<3; i++) {
-    digitalWrite(mouth[i], HIGH);
-    delay(750);
-  }
+void eyesOn() {
   digitalWrite(eyes, HIGH);
+}
+
+void eyesOff() {
+  digitalWrite(eyes, HIGH);
+}
+
+void animateMouth(int volume) {
+  const int thresholds[3] = {10, 45, 100};
+
+  for(int i=0; i<3; i++) {
+    if(volume > thresholds[i])
+      digitalWrite(mouth[i], HIGH);
+    else
+      digitalWrite(mouth[i], LOW);
+  }
+}
+
+
+// get measure of volume from wave shield
+int getVolume(int volumePin)
+{
+  int volume, v, i;
+  volume = 0;
+  for(i=0; i<8; i++) {
+    v = analogRead(volumePin) - 512;
+    if(v < 0) v *= -1;
+    if(v > volume) volume = v;
+    delay(5);
+  }
+  return(volume-200);
+}
+
+
+// open and start playing a file
+void playfile(char *name) {
+  if (wave.isplaying) // already playing something
+    wave.stop(); // stop it
+
+  if(!file.open(root, name))
+    return;
+
+  if(!wave.create(file))
+    return;
+
+  wave.play();
 }
